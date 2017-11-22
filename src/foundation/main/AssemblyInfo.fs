15ec07a5c91ee573a7f372b08261b6366da2db04
ivaylo5ev <ivaylo5ev@gmail.com>
2017-11-22 13:00:49 +0200

    - migrated verman targets to new mason backend

--- src/foundation/main/AssemblyInfo.fs
+++ src/foundation/main/AssemblyInfo.fs
@@ -18,8 +18,8 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("f46be8de-7918-48af-b19f-4d33a4fd6d79")>]
 
-[<assembly: AssemblyVersion("0.2.0.173")>]
-[<assembly: AssemblyFileVersion("0.2.0.173")>]
+[<assembly: AssemblyVersion("0.2.0.177")>]
+[<assembly: AssemblyFileVersion("0.2.0.177")>]
 
 do
     ()
