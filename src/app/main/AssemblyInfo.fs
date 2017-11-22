79225e5a68a97e7d73d260af0495b030ffcbe4e5
ivaylo5ev <ivaylo5ev@gmail.com>
2017-11-22 12:36:45 +0200

    ...

--- src/app/main/AssemblyInfo.fs
+++ src/app/main/AssemblyInfo.fs
@@ -18,8 +18,8 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("7a94252c-7fc4-465d-92fa-1282bf322e15")>]
 
-[<assembly: AssemblyVersion("0.2.0.84")>]
-[<assembly: AssemblyFileVersion("0.2.0.84")>]
+[<assembly: AssemblyVersion("0.2.0.92")>]
+[<assembly: AssemblyFileVersion("0.2.0.92")>]
 
 do
     ()
--- src/foundation/main/Sdk/MasonRunner.fs
+++ src/foundation/main/Sdk/MasonRunner.fs
@@ -21,18 +21,27 @@ module MasonRunner =
     let filterAssemblies files: seq<string> =
         files |> Seq.filter (fun f -> not (_expression.IsMatch(Path.GetFileName(f)) ) )
 
+    let loadAssembly file =
+        try 
+            Some(Assembly.LoadFrom(file)) 
+        with 
+        | _ -> None
+
     let getAssemblies() =
         let probingDirectories: string list = [
             AppDomain.CurrentDomain.BaseDirectory;
             ]
         probingDirectories 
         |> Seq.map (fun d -> Directory.EnumerateFiles(d, AssemblySearchPattern) |> filterAssemblies)
-        |> Seq.collect (fun files -> files |> Seq.map (fun f -> try Some(Assembly.LoadFrom(f)) with | _ -> None))
+        |> Seq.collect (fun files -> files |> Seq.map loadAssembly)
         |> Seq.choose id |> Seq.toList
+
+    let isModuleType (t: Type) = 
+        t.IsClass && not t.IsAbstract && typedefof<IMasonModule>.IsAssignableFrom(t)
 
     let getPluginTypes (asm: Assembly) = 
         asm.GetTypes() 
-        |> Seq.filter (fun t -> t.IsClass && not t.IsAbstract && typedefof<IMasonModule>.IsAssignableFrom(t))
+        |> Seq.filter isModuleType
 
     let instantiateType (t: Type) = 
         Activator.CreateInstance(t) :?> IMasonModule
@@ -44,15 +53,16 @@ module MasonRunner =
 
     let Run(optionParser: IOptionParser, args) = 
         let modules = getModules();
+        printfn "Modules count %i" (Array.ofSeq modules).Length
         match List.ofArray args with
         | [] | [_] -> 
-            System.Console.WriteLine("Insufficient arguments")
+            printfn "Insufficient arguments"
             ()
         | candidateModule::remainder ->
             let cmp = StringComparer.OrdinalIgnoreCase;
             match modules |> Seq.tryFind (fun m -> cmp.Equals(m.Name, candidateModule)) with 
             | Some m -> 
-                System.Console.WriteLine("Executing module {0}", m.Name)
+                printfn "Executing module %s" m.Name
                 // TODO: let the module itself determine if a project name was specified
                 let projectName = remainder.Head
                 let options = optionParser.Parse(Array.ofList remainder.Tail)
